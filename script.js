ea303d4029d4a83b1ecfc62192329e52faf6de15
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-08-27 00:43:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function filterNames(){
     for(let i = 0; i < li.length; i++){
         let a = li[i].getElementsByTagName('a')[0];
         
-     // If matched
+   // If matched
         if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
             li[i].style.display = '';
         } else {
