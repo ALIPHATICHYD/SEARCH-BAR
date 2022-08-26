7542c64cb6f97af069b015b4fecbfdbbe5b4da10
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-08-27 00:42:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function filterNames(){
       // Loop through collection-item list
     for(let i = 0; i < li.length; i++){
         let a = li[i].getElementsByTagName('a')[0];
+     // If matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+
+        }
+    }
+
+}
